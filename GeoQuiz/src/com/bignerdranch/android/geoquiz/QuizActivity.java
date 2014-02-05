1d2583d068f2bb31fe5119e56728b5e3b9fe3ec5
Caroline Taymor <ctaymor@gmail.com>
2014-02-04 17:08:28 -0800

    controller code in QuizActivity.java and ids for true and false buttons Created variables mTrueButton and mFalseButton, and wrote onClickListeners for them

--- GeoQuiz/src/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ GeoQuiz/src/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -1,22 +1,44 @@
 package com.bignerdranch.android.geoquiz;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
 
 public class QuizActivity extends Activity {
+    private Button mTrueButton;
+    private Button mFalseButton;
 
-	@Override
+    @Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_quiz);
-	}
+	    super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.quiz, menu);
-		return true;
-	}
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //will do something eventually
+            }
+        });
+			
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //will do something eventually
+            }
+        });
+		
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.quiz, menu);
+        return true;
+    }
 
 }
