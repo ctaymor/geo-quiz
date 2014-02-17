114d211bbde20f209c5fc183679a2873ebcbd5d5
ctaymor <ctaymor@gmail.com>
2014-02-16 17:02:51 -0800

    Getting GeoQuiz set up without challenges for Lab 3 Some files were not added to all commits in Lab 2, so the roll back required added some of the newer stuff.

--- GeoQuiz/src/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ GeoQuiz/src/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.geoquiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +17,7 @@ import android.widget.Toast;
  * @author Caroline Taymor
  */
 public class QuizActivity extends Activity {
+    // TODO: What is proper order for variables?
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
@@ -31,11 +33,13 @@ public class QuizActivity extends Activity {
 
     private int mCurrentIndex = 0;
     
+    //private static final String TAG = "Quiz Activity";
+    
     @Override
 	protected void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
-        mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
         
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener(){
