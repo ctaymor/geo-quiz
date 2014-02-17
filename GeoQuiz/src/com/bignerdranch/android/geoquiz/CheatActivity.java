d00756c1f5ead5d50a8f2ef9b0a0d8610933b46f
ctaymor <ctaymor@gmail.com>
2014-02-17 12:58:54 -0800

    Added intent extra to intent passed for opening CheatActivity which passes true or false answer to question

--- GeoQuiz/src/com/bignerdranch/android/geoquiz/CheatActivity.java
+++ GeoQuiz/src/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -4,10 +4,16 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class CheatActivity extends Activity {
+    public boolean mAnswerIsTrue;
+    public static final String EXTRA_ANSWER_IS_TRUE = 
+            "com.bignerdranch.android.geoquiz.answer_is_true";
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
+        
+        mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
     }
 
 }
